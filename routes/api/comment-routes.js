72e4b83c5d739ea9adff4e8503436729cef72920
brookemadison <brookeperry611@gmail.com>
2021-10-23 12:14:21 -0400

    Reply Functionality with the Front End

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,14 +1,26 @@
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+  } = require('../../controllers/comment-controller');
+  
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment)
 
 const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
+
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
 
 router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
